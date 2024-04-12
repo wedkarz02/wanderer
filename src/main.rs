312b2a2c7462beeff542364e9c96050515697e2a
Paweł Rybak <87527837+wedkarz02@users.noreply.github.com>
2024-04-12 23:00:38 +0200

    matrix multiplication function

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,28 @@
+use std::process;
+
 pub mod matrix;
 
 fn main() {
-    let mut mat = matrix::Matrix::from_size(5, 10);
+    let mut mat = matrix::Matrix::from_size(2, 3);
+    let mut other = matrix::Matrix::from_size(3, 2);
+    mat.rows[0][0] = 1f64;
+    mat.rows[0][1] = 4f64;
+    mat.rows[0][2] = 2f64;
+    mat.rows[1][0] = 2f64;
+    mat.rows[1][1] = 5f64;
+    mat.rows[1][2] = 1f64;
 
-    for i in 0..mat.rows.len() {
-        mat.rows[i][i] = 1.0;
-    }
+    other.rows[0][0] = 2f64;
+    other.rows[0][1] = 1f64;
+    other.rows[1][0] = 1f64;
+    other.rows[1][1] = 1f64;
+    other.rows[2][0] = 3f64;
+    other.rows[2][1] = 2f64;
 
-    println!("{}", mat);
+    let res = mat.multiply(&other).unwrap_or_else(|err| {
+        eprintln!("{}", err);
+        process::exit(1);
+    });
+
+    println!("{}", res);
 }
--- src/matrix.rs
+++ src/matrix.rs
@@ -1,5 +1,23 @@
+#[derive(Debug)]
+pub struct SizeError;
+
+impl std::fmt::Display for SizeError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "invalid matrix size")
+    }
+}
+
 pub struct Matrix {
     pub rows: Vec<Vec<f64>>,
+}
+
+impl std::fmt::Display for Matrix {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for row in &self.rows {
+            writeln!(f, "{:4?}", row)?
+        }
+        Ok(())
+    }
 }
 
 impl Matrix {
@@ -9,16 +27,25 @@ impl Matrix {
 
     pub fn from_size(rows: usize, cols: usize) -> Self {
         Self {
-            rows: vec![vec![0.0; cols]; rows],
+            rows: vec![vec![0f64; cols]; rows],
         }
     }
-}
 
-impl std::fmt::Display for Matrix {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        for row in &self.rows {
-            writeln!(f, "{:?}", row)?
+    pub fn multiply(&self, other: &Self) -> Result<Self, SizeError> {
+        if self.rows[0].len() != other.rows.len() {
+            return Err(SizeError);
         }
-        Ok(())
+
+        let mut out = Self::from_size(self.rows.len(), other.rows[0].len());
+
+        for i in 0..self.rows.len() {
+            for j in 0..other.rows[0].len() {
+                for k in 0..self.rows[0].len() {
+                    out.rows[i][j] += self.rows[i][k] * other.rows[k][j];
+                }
+            }
+        }
+
+        Ok(out)
     }
 }
