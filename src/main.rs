10ad2136e84bc9b21337d6078b47d15f8ac98553
Paweł Rybak <87527837+wedkarz02@users.noreply.github.com>
2024-04-13 13:27:53 +0200

    jacobi linear equation solving function

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,11 @@
-use std::process;
-
 pub mod matrix;
 
 fn main() {
-    let mut mat = matrix::Matrix::from_size(2, 3);
-    let mut other = matrix::Matrix::from_size(3, 2);
-    mat.rows[0][0] = 1f64;
-    mat.rows[0][1] = 4f64;
-    mat.rows[0][2] = 2f64;
-    mat.rows[1][0] = 2f64;
-    mat.rows[1][1] = 5f64;
-    mat.rows[1][2] = 1f64;
+    let mat = matrix::Matrix::from_vecs(vec![vec![2.0, 1.0], vec![5.0, 7.0]]);
 
-    other.rows[0][0] = 2f64;
-    other.rows[0][1] = 1f64;
-    other.rows[1][0] = 1f64;
-    other.rows[1][1] = 1f64;
-    other.rows[2][0] = 3f64;
-    other.rows[2][1] = 2f64;
+    let b = vec![11.0, 13.0];
+    let x = vec![1.0; b.len()];
 
-    let res = mat.multiply(&other).unwrap_or_else(|err| {
-        eprintln!("{}", err);
-        process::exit(1);
-    });
-
-    println!("{}", res);
+    println!("{}\n{:?}\n\n{:?}", mat, b, x);
+    println!("{:?}", mat.jacobi(&b, &x, 25));
 }
--- src/matrix.rs
+++ src/matrix.rs
@@ -1,9 +1,13 @@
 #[derive(Debug)]
-pub struct SizeError;
+pub enum MatrixError {
+    SizeError,
+}
 
-impl std::fmt::Display for SizeError {
+impl std::fmt::Display for MatrixError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        writeln!(f, "invalid matrix size")
+        match self {
+            Self::SizeError => writeln!(f, "invalid matrix size"),
+        }
     }
 }
 
@@ -22,7 +26,7 @@ impl std::fmt::Display for Matrix {
 
 impl Matrix {
     pub fn new() -> Self {
-        Self { rows: vec![vec![]] }
+        Self { rows: vec![] }
     }
 
     pub fn from_size(rows: usize, cols: usize) -> Self {
@@ -31,9 +35,28 @@ impl Matrix {
         }
     }
 
-    pub fn multiply(&self, other: &Self) -> Result<Self, SizeError> {
+    pub fn from_vecs(vec_matrix: Vec<Vec<f64>>) -> Self {
+        Self { rows: vec_matrix }
+    }
+
+    pub fn init_default_path(size: usize) -> Self {
+        let mut out = Self::from_size(size, size);
+
+        out.rows[0][0] = 1f64;
+        out.rows[size - 1][size - 1] = 1f64;
+
+        for i in 1..size - 1 {
+            out.rows[i][i - 1] = -0.5;
+            out.rows[i][i] = 1f64;
+            out.rows[i][i + 1] = -0.5;
+        }
+
+        out
+    }
+
+    pub fn multiply(&self, other: &Self) -> Result<Self, MatrixError> {
         if self.rows[0].len() != other.rows.len() {
-            return Err(SizeError);
+            return Err(MatrixError::SizeError);
         }
 
         let mut out = Self::from_size(self.rows.len(), other.rows[0].len());
@@ -47,5 +70,27 @@ impl Matrix {
         }
 
         Ok(out)
+    }
+
+    pub fn dot_product(x: &[f64], y: &[f64]) -> f64 {
+        return x.iter().zip(y.iter()).map(|(&a, &b)| a * b).sum();
+    }
+
+    // https://en.wikipedia.org/wiki/Jacobi_method
+    pub fn jacobi(&self, b: &Vec<f64>, x0: &Vec<f64>, max_iter: usize) -> Vec<f64> {
+        let mut x = x0.to_vec();
+        let mut x_new = vec![0.0; self.rows.len()];
+
+        for _ in 0..max_iter {
+            for i in 0..self.rows.len() {
+                x_new[i] = (b[i]
+                    - Self::dot_product(&self.rows[i][..i], &x[..i])
+                    - Self::dot_product(&self.rows[i][i + 1..], &x[i + 1..]))
+                    / self.rows[i][i];
+            }
+            x = x_new.clone();
+        }
+
+        x_new
     }
 }
